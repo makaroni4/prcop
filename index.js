df9716b2fc84287c5dbcb66b89f6e5ce67ed81fd
Anatoli Makarevich <makaroni4@gmail.com>
2021-10-21 08:35:21 +0200

    Actually linting PR title

--- index.js
+++ index.js
@@ -19,13 +19,15 @@ try {
   console.log("--> titleRegexp: ", titleRegexp);
   console.log("--> titleErrorMessage: ", titleErrorMessage);
 
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  const regexp = new RegExp(titleRegexp);
+
+  if (regexp.test(prTitle)) {
+    core.info("Your PR title is perfect!");
+  } else {
+    core.setFailed(titleErrorMessage);
+  }
+
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
