e2de4dd164e3fd11524b437432aa4cb4a962c35c
Anatoli Makarevich <makaroni4@gmail.com>
2021-10-24 11:10:11 +0200

    Debugging

--- src/read-config.js
+++ src/read-config.js
@@ -1,5 +1,8 @@
 module.exports = async (octokit, pr, configFilePath) => {
   const [owner, repo] = pr.repoFullName.split("/");
+
+  console.log(owner, repo, configFilePath);
+
   const response = await octokit.repos.getContent({
     owner: owner,
     repo: repo,
