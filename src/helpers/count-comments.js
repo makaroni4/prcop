70f0184b6d4dafa832744ea5a83fbc4085fe96f1
Anatoli Makarevich <makaroni4@gmail.com>
2021-10-24 11:46:01 +0200

    Add ESlint

--- src/helpers/count-comments.js
+++ src/helpers/count-comments.js
@@ -2,9 +2,7 @@ module.exports = async (octokit, pr) => {
   // PR number is in URL like 4821 https://github.com/foo/bar/pull/4821
   const response = await octokit.request(`GET /repos/${pr.repoFullName}/pulls/${pr.number}/comments`);
 
-  const authorComments = response.data.filter(comment => {
-    return comment.user.login === pr.authorLogin;
-  });
+  const authorComments = response.data.filter(comment => comment.user.login === pr.authorLogin);
 
   return authorComments.length;
 };
